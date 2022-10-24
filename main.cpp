41629a443b863228f9c593ddf5b4ca5313c422df
LFY10 <73308332+LFY10@users.noreply.github.com>
2022-10-23 21:21:54 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//main.cpp, put your driver code here,
+//main.cpp, put your driver code here, 
 //you can manipulate your class objects here
 #include <iostream> //cin and cout
 #include <fstream> //file processing
@@ -8,11 +8,16 @@
 #include "stu_sort.hpp"
 using namespace std;
 
-/*I provide example code here to help you read the input
- *data from a file, so that you can focus on creating
- *and manipulating classes and objects
- */
+
 int main(){
+
+  // two arrays to hold informations
+  DomesticStudent domesArr[100];
+  InternationalStudent interArr[100];
+
+  // store toefl grade and merge them to interArr[]
+  ToeflScore toeflArr[100];
+
   //Read the domestic-stu.txt file and exit if failed
   string line;
   ifstream domesticFile("domestic-stu.txt");
@@ -22,89 +27,67 @@ int main(){
     cout << "Unable to open file domestic-stu.txt" << endl;
     return -1;
   }
- 
- 
+  
+  
   if(!internationalFile.is_open()) {
     cout << "Unable to open file international-stu.txt" << endl;
     return -1;
   }
- 
+  
 
   //Read the first line of domestic-stu.txt, which specifies
   //the file format. And then print it out to the screen
   getline(domesticFile, line);
   getline(internationalFile, line);
-  int stu_count = 1;
-  int int_count = 1;
+  int stu_count = 0;
+  int int_count = 0;
  
-  // five domestic arrays
-  string domFir[100] = {};
-  string domLas[100] = {};
-  string domPro[100] = {};
-  float domCGPA[100] = {};
-  int domRes [100] = {};
-
-  // nine international arrays
-  string intFir[100] = {};
-  string intLas[100] = {};
-  string intCou[100] = {};
-  float intCGPA[100] = {};
-  int intRes [100] = {};
-  int toeRea [100] = {};
-  int toeLis [100] = {};
-  int toeSpe [100] = {};
-  int toeWri [100] = {};
-
 //domestic arrays
   while( getline(domesticFile, line) ) {
-    /*process each line, get each field separated by a comma.
-     *We use istringstream to handle it.
-     *Note in this example code here, we assume the file format
-     *is perfect and do NOT handle error cases. We will leave the
-     *error and exception handling of file format to Lab Assignment 4
-     */
+    
     istringstream ss(line);
 
     string firstName, lastName, province, s_cgpa, s_researchScore;
     float cgpa;
     int researchScore;
-   
-   
+    
+    
     //get firstName separated by comma
     getline(ss, firstName, ',');
-    domFir[stu_count-1] = firstName;
+    domesArr[stu_count].setFirstName(firstName);
+    
    
 
     //get lastName separated by comma
     getline(ss, lastName, ',');
-    domLas[stu_count-1] = lastName;
+    domesArr[stu_count].setLastName(lastName);
+    
 
     //get province separated by comma
     getline(ss, province, ',');
-    domPro[stu_count-1] = province;
+    domesArr[stu_count].setProvince(province);
+    
 
     //get cpga separated by comma, and convert string to float
     getline(ss, s_cgpa);
     cgpa = atof(s_cgpa.c_str());
-    domCGPA[stu_count-1] = cgpa;
-   
+    domesArr[stu_count].setCGPA(cgpa);
+    
+    
     //get researchScore separated by comma, and convert it to int
     getline(ss, s_researchScore, ',');
     researchScore = atoi(s_researchScore.c_str());
-    domRes[stu_count-1] = researchScore;
+    domesArr[stu_count].setResearchScore(researchScore);
+    
 
-    //print the student info to the screen
-   /* cout << "Domestic student " << stu_count << " " << firstName << " "
-<< lastName << " from " << province << " province has cgpa of "
-<< cgpa << ", and research score of " << researchScore << endl;
-*/
+    
     stu_count++;
   }
 
 
 // international arrays
 while( getline(internationalFile, line) ) {
-   
+    
     istringstream ss(line);
 
     string firstName, lastName, country, s_cgpa, s_researchScore, s_read, s_listen, s_speak, s_write;
@@ -114,59 +97,61 @@ while( getline(internationalFile, line) ) {
     int listen;
     int speak;
     int write;
-   
-   
+    
+    
     //get firstName separated by comma
     getline(ss, firstName, ',');
-    intFir[int_count-1] = firstName;
+    interArr[int_count].setFirstName(firstName);
    
 
     //get lastName separated by comma
     getline(ss, lastName, ',');
-    intLas[int_count-1] = lastName;
+    interArr[int_count].setLastName(lastName);
 
     //get province separated by comma
     getline(ss, country, ',');
-    intCou[int_count-1] = country;
+    interArr[int_count].setCountry(country);
 
     //get cpga separated by comma, and convert string to float
     getline(ss, s_cgpa);
     cgpa = atof(s_cgpa.c_str());
-    intCGPA[int_count-1] = cgpa;
-   
+    interArr[int_count].setCGPA(cgpa);
+    
     //get researchScore separated by comma, and convert it to int
     getline(ss, s_researchScore, ',');
     researchScore = atoi(s_researchScore.c_str());
-    intRes[int_count-1] = researchScore;
+    interArr[int_count].setResearchScore(researchScore);
 
     getline(ss, s_read);
     read = atof(s_read.c_str());
-    toeRea[int_count-1] = read;
-   
+    toeflArr[int_count].setReading(read);
+    
     getline(ss, s_listen);
     listen = atof(s_listen.c_str());
-    toeLis[int_count-1] = listen;
-   
+    toeflArr[int_count].setListening(listen);
+    
     getline(ss, s_speak);
     speak = atof(s_speak.c_str());
-    toeSpe[int_count-1] = speak;
-   
+    toeflArr[int_count].setSpeaking(speak);
+    
     getline(ss, s_write);
     write = atof(s_write.c_str());
-    toeWri[int_count-1] = write;
-   
+    toeflArr[int_count].setWriting(write);
+    
+    interArr[int_count].setToeflScore(toeflArr[int_count]);
 
     int_count++;
   }
 
-
+/*
 //test
   for (int i = 0; i < 100; i++){
-
-  cout << intCGPA[i]<<endl;
+	
+  	cout << domesArr[i]<<endl;
   }
   //close your file
   domesticFile.close();
   internationalFile.close();
+*/
   return 0;
 }
